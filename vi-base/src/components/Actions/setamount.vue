d474694b1dbaf012fd22b37ca2bfa8922055c4b4
Andreas H. Kelch <ak@mausbrand.de>
2023-01-20 09:44:26 +0100

    fix: sl-select uses now options

--- vi-base/src/components/Actions/setamount.vue
+++ vi-base/src/components/Actions/setamount.vue
@@ -1,8 +1,8 @@
 <template>
   <sl-select value="30" :label='$t("actions.setamount") + ":" ' @sl-change="amoutChange" size="small">
-    <sl-menu-item value="30">30</sl-menu-item>
-    <sl-menu-item value="60">60</sl-menu-item>
-    <sl-menu-item value="99">99</sl-menu-item>
+    <sl-option value="30">30</sl-option>
+    <sl-option value="60">60</sl-option>
+    <sl-option value="99">99</sl-option>
   </sl-select>
 
 </template>
