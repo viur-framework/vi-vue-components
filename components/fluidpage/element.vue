47f37cf156ade28339e28eee9aa4fcc4712ee666
Andreas H. Kelch <>
2024-10-02 15:11:53 +0200

    fix: fluidpages for non multiples

--- components/fluidpage/element.vue
+++ components/fluidpage/element.vue
@@ -30,7 +30,7 @@
       </div>
 
 
-      <sl-select placeholder="Sprache" size="small" @sl-change="changeLang" class="langchooser" :value="state.lang"
+      <sl-select v-if="state.languages && self.languages.length>0" placeholder="Sprache" size="small" @sl-change="changeLang" class="langchooser" :value="state.lang"
         :class="{ 'is-selected': handlerState.currentSelection?.[0]['key'] === skel['key'] }"
       >
           <sl-option v-for="lang in state.languages" :value="lang" :key="lang">
@@ -182,7 +182,7 @@ export default {
       }),
       lang:"de",
       languages:computed(()=>{
-        return currentlist.structure['headline']['languages']
+        return currentlist.structure['headline']?.['languages']
       })
     })
     const changeOrder = inject("changeOrder")
