e959b8c6ee4f10a0de3b7964ec3d7c4fcf1580cd
Andreas H. Kelch <32266000+akelch@users.noreply.github.com>
2025-02-18 14:13:13 +0100

    fix: scriptor runner scrolling

--- src/extensions/scriptor/ScriptorRunner.vue
+++ src/extensions/scriptor/ScriptorRunner.vue
@@ -36,22 +36,26 @@
           :label="current['rel']['name']"
           @sl-after-hide="state.opened=false"
         >
+          <div class="wrapper-widgets" ref="messagewrapper">
         <status-bar :id="state.id" :filename="current['dest']['name']"></status-bar>
+
         <widget-list :id="state.id"></widget-list>
+          </div>
 
         </sl-dialog>
       </teleport>
 </template>
 
 <script setup>
-  import {onBeforeMount, reactive, ref, computed, inject} from 'vue';
+import {onBeforeMount, reactive, ref, computed, inject, watch} from 'vue';
   import WidgetList from './components/WidgetList.vue';
   import StatusBar from './components/StatusBar.vue';
   import Status from './components/Status.vue';
   import {useScriptorStore} from "./store/scriptor"
   import { Request } from '@viur/vue-utils';
   import Utils from '../../utils';
-
+import {useDebounceFn} from "@vueuse/core";
+const messagewrapper = ref(null)
   const scriptorAction = ref(null)
 
   const handlerState = inject("handlerState")
@@ -101,9 +105,25 @@
     scriptorAction.value.executeScript()
   }
 
+  watch(
+    () => state.scriptor?.messages.length,
+    (newVal, oldVal) => {
+      if (messagewrapper.value){
+        const scroller = useDebounceFn((event) => {
+          messagewrapper.value.scrollTop = 999999
+        }, 1)
+        scroller()
+      }
+
+    }
+  )
+
 </script>
 
 <style scoped>
+.wrapper-widgets{
+  overflow-y: auto;
+}
 .tabpopup {
   &::part(base) {
     position: absolute;
@@ -117,7 +137,8 @@
   }
 
   &::part(body) {
-    display: contents;
+    padding:0;
+    /*display: contents;*/
   }
 
   &::part(footer) {
