bd87c2089c3338f677703f0a2185a699d50862a9
Andreas H. Kelch <32266000+akelch@users.noreply.github.com>
2025-05-07 16:27:56 +0200

    fix: update admin widgets

--- src/init.js
+++ src/init.js
@@ -69,13 +69,13 @@ export function useInitConnection() {
 
     function initBones() {
       addBoneWidget("select.access", selectaccessBone) //add Bone to store
-      addBoneWidget("relational.tree.leaf.file.file", fileBone) //add Bone to store
-      addBoneWidget("relational.tree.leaf.file.file.", fileBone) //add Bone to store
+      addBoneWidget("relational.tree.leaf.file.", fileBone) //add Bone to store
       addBoneWidget("relational.", relationalBone) //add Bone to store
 
-      addBoneActionbar("relational.tree.leaf.file.file", fileBar) //add custom multiple acionbar
-      addBoneActionbar("relational.tree.leaf.file.file.", fileBar) //add custom multiple acionbar
+
+      addBoneActionbar("relational.tree.leaf.file.", fileBar) //add custom multiple acionbar
       addBoneActionbar("relational.", relationalBar) //add custom multiple acionbar
+
     }
 
     function initExtensions() {
