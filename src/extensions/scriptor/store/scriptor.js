e7d5cc3fcca4725a8610bf45b37781a839cce7c9
Arne Gudermann <ag@mausbrand.de>
2025-08-21 16:48:04 +0200

    fix: tabId for setParams in scriptor if the key has an underscore

--- src/extensions/scriptor/store/scriptor.js
+++ src/extensions/scriptor/store/scriptor.js
@@ -119,7 +119,8 @@ export const useScriptorStore = defineStore("scriptorStore", () => {
   async function setParams(scriptParams = {}) {
     const contextStore = useContextStore()
     //Use window object, because useRoute not work outside module.
-    const tabId = (window.location.hash || "").replace(/^#/, "").split("_")[1].replace("=", "")
+    const urlData =(window.location.hash || "").replace(/^#/, "").split("_")
+    const tabId = urlData[urlData.length-1].replace("=", "")
     let selectedEntries = contextStore.getLocalContext(tabId, true)["_selectedEntries"]
     if (!selectedEntries && !scriptParams) {
       return
